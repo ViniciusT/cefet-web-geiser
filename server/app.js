18433e38641e22c2eb9473055e4ec1b8f9c4d3d8
Vinicius Teodoro <vinicius.teodoro@enacom.com.br>
2017-05-09 12:05:24 -0300

    aula

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 var express = require('express'),
     app = express();
+var fs = require('fs');
+var http = require('http');
+app.set('view engine', 'hbs');
+app.set('views', 'server/views');
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
 var db = {
+    jogador: JSON.parse(fs.readFileSync('/Users/vinicius/Desktop/cefet-geiser/cefet-web-geiser/server/data/jogadores.json')),
+    jogosPJogador: JSON.parse(fs.readFileSync('/Users/vinicius/Desktop/cefet-geiser/cefet-web-geiser/server/data/jogosPorJogador.json'))
+
 };
 
 
@@ -28,6 +35,15 @@ var db = {
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
+
+app.use(express.static('/Users/vinicius/Desktop/cefet-geiser/cefet-web-geiser/client'));
+app.get('/', function(request, response) {
+    response.render('index.hbs', db.jogador);
+});
+let server = app.listen(3000, function () {
+    console.log(db.jogador);
+});
+
 
 // abrir servidor na porta 3000
 // dica: 1-3 linhas de código
